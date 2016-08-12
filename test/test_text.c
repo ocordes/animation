09b676fbc45dcb7f3d0c920fc309d69119d5d002
Oliver Cordes <ocordes@astro.uni-bonn.de>
2016-08-12 09:34:23 +0200

    - more tests

--- test/test_text.c
+++ test/test_text.c
@@ -5,6 +5,12 @@
 
 #include <wand/magick_wand.h>
 
+/*
+
+gcc -o test_text test_text.c -I/sw/include/GraphicsMagick -L/sw/lib/graphicsmagick1322 -lGraphicsMagickWand -lGraphicsMagick
+
+*/
+
 int main( int argc, char *argv[] )
 {
   MagickWand          *im;
@@ -28,13 +34,15 @@ int main( int argc, char *argv[] )
   else
   {
     printf( "MagickPass=%i MagickFail=%i\n", MagickPass, MagickFail );
+    printf( "True=%i False=%i\n", True, False );
     im = NewMagickWand();
 
     if ( im == NULL )
       printf( "NULL pointer\n" );
 
     //result = MagickReadImage( im, "olli.jpg" );
-    MagickSetImagePage( im, width, height, 0, 0 );
+    //MagickSetImagePage( im, width, height, 0, 0 );
+    MagickSetSize( im, width, height );
     result = MagickSetImagePixels( im, 0, 0, width, height, "RGB", CharPixel, pixels );
     free( pixels );
 

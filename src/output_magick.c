245f09a0293fcd43148d023b8b8dbd0426e45466
Oliver Cordes <ocordes@astro.uni-bonn.de>
2016-08-14 19:44:37 +0200

    - change the output write infrastructure

---
+++ src/output_magick.c
@@ -0,0 +1,27 @@
+/* (C) Copyright 2010 by Oliver Cordes
+        - ocordes ( at ) astro ( dot ) uni-bonn ( dot ) de
+
+
+    This file is part of animation.
+
+    animation is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    animation is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with animation.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/* output_magick.c
+
+   writtem by: Oliver Cordes 2016-08-14
+   changed by: Oliver Cordes 2016-08-14
+
+*/
